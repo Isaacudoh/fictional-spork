62ce0841d6fa3d58b129f81faf4c5188013700d8
nsikakudoh <nsikak@sparknerds.io>
2023-08-31 12:47:45 -0600

    visible form on click

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
 
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+  }
+});
